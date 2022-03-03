b74ba83c61bc9a079941b26d9e3c3d3d73f05326
Wei-Way <a1801549@student.adelaide.edu.au>
2022-03-03 16:16:53 +1030

    Updated version to check for compilation error

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include "player.h"
+/*#include "player.h"
 #include "com.h"
-#include "ref.h"
+#include "ref.h"*/
 
 int main(){
-    
+    std::cout << "Hello Year 2!" << std::endl;
 
 
-
+    return 0;
 }
